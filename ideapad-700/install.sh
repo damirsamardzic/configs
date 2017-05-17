64c1505df066d9c458a1bc19e0af98cfbe823612
Damir S <damirsamardzic@gmail.com>
2017-05-18 00:13:16 +0200

    Added ideapad 700 laptop

---
+++ ideapad-700/install.sh
@@ -0,0 +1,30 @@
+echo Installing all packages...
+
+echo Entering sudo...
+sudo -i
+  echo "HandleLidSwitch=ignore" >> /etc/systemd/logind.conf
+  pacman -Sy
+  pacman -S $( cat pacman-installed )
+
+exit
+
+echo Copying config files..
+
+cp -f ~/.config/i3/config ~/.config/i3/config.orig
+cp -f i3-config ~/.config/i3/config
+
+mkdir -p ~/.config/dunst
+cp -f ~/.config/dunst/dunstrc ~/.config/dunst/dunstrc.orig
+cp -f dunstrc ~/.config/dunst/dunstrc
+
+mkdir -p ~/.config/i3status
+cp -f i3status-config ~/.config/i3status/config
+
+cp -f Xdefaults ~/.Xdefaults
+
+cp -f Xresources ~/.Xresources
+
+cp -f xinitrc ~/.xinitrc
+
+echo Done.
+
