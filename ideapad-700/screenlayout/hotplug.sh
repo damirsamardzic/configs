1bf5e9e83204f165abb9b5df7bbedf19e2a5bcf0
Damir Samardzic <damir.samardzic@sartura.hr>
2017-06-29 09:06:38 +0200

    Updated hotplug for monitors handling

--- ideapad-700/screenlayout/hotplug.sh
+++ ideapad-700/screenlayout/hotplug.sh
@@ -10,12 +10,24 @@ LOG=/tmp/hotplug.log
 /usr/bin/sleep 2
 
 /bin/date 2>&1 >> $LOG
-if [[ $(cat /sys/class/drm/card0-HDMI-A-1/status | grep -Ec "^connected") -eq 1 ]]; then
-        echo "HDMI connected!" >> $LOG
-        /usr/bin/sleep 2
-        /home/damirs/.screenlayout/laptop-monitor.sh >> $LOG 2>&1
-else
-        echo "HDMI disconnected!" >> $LOG
-        /home/damirs/.screenlayout/laptop-only.sh >> $LOG 2>&1
-fi
+
+#if [[ $(cat /sys/class/drm/card0-HDMI-A-1/status | grep -Ec "^connected") -eq 1 ]]; then
+#        echo "HDMI connected!" >> $LOG
+#        /usr/bin/sleep 1
+#        /home/damirs/.screenlayout/laptop-monitor.sh >> $LOG 2>&1
+#else
+#        echo "HDMI disconnected!" >> $LOG
+#        /home/damirs/.screenlayout/laptop-only.sh >> $LOG 2>&1
+#fi
+
+function connect(){
+    /home/damirs/.screenlayout/laptop-monitor.sh >> $LOG 2>&1
+}
+
+function disconnect(){
+    /home/damirs/.screenlayout/laptop-only.sh >> $LOG 2>&1 
+}
+
+
+xrandr | grep "HDMI-1 connected" &> /dev/null && connect || disconnect
 
